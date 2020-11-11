b7d1a9dfe0b27459c15f2499ff8e99dbc0b1af76
Beranger <beranger.n@protonmail.com>
2020-11-11 15:44:37 +0100

    Design input, improve UX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import './App.css'
 
 import React, { Component } from 'react'
 
 import axios from 'axios'
 
-const URL = 'https://us-central1-medium-76adb.cloudfunctions.net/checkTemperature?temperature='
+import LinearProgress from '@material-ui/core/LinearProgress'
+import { Typography } from '@material-ui/core'
+import { URL } from './config'
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +14,8 @@ class App extends Component {
 
     this.state = {
       message: '',
-      temperature: ''
+      temperature: '',
+      loading: false,
     }
   }
 
@@ -21,40 +23,86 @@ class App extends Component {
   getDiagnosis = async () => {
     const { temperature } = this.state
 
+    this.setState({ loading: true, message: '' })
+
     const baseUrl = `${URL}${temperature}`
 
-    const resultGetDiagnosis = await axios.get(baseUrl) //Call the Cloud Function
+    try {
+      const resultGetDiagnosis = await axios.get(baseUrl) //Call the Cloud Function
 
-    if (resultGetDiagnosis.data && resultGetDiagnosis.data.status === 200) {
-      this.setState({ message: resultGetDiagnosis.data.message })
-    } else {
-      console.log(resultGetDiagnosis)
+      if (resultGetDiagnosis.data && resultGetDiagnosis.data.status === 200) {
+        this.setState({ message: resultGetDiagnosis.data.message })
+      } else {
+        console.log(resultGetDiagnosis)
+      }
+    } catch (err) {
+      console.log('ERROR', err)
+      this.setState({ message: 'An internal error happened.' })
     }
+    this.setState({ loading: false })
   }
 
   //Function called when inserting a value
   handleChange = ev => {
     const { value, name } = ev.target
-    this.setState({ [name]: value })
+
+    if (value.match(/\d/g))
+      this.setState({ [name]: value.match(/\d/g).join('') })
+    else this.setState({ [name]: '' })
+  }
+
+  //When user press Enter
+  handleKeyDown = ev => {
+    if (ev.key === 'Enter') {
+      this.getDiagnosis()
+    }
   }
 
   render() {
-    const { message, temperature } = this.state
+    const { message, temperature, loading } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            <input type="text" name='temperature' value={temperature} onChange={this.handleChange} placeholder='Your temperature' />
-            <br /><br />
-            <button onClick={this.getDiagnosis}>
-              Get diagnosis
-            </button>
-            <br /><br />
-            {message}
-          </p>
+      <div className='App'>
+        <header className='App-header'>
+          <div>
+            <input
+              type='text'
+              name='temperature'
+              value={temperature}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyDown}
+              placeholder='Your temperature'
+              autoFocus={true}
+              style={{
+                textAlign: 'center',
+                background: 'none',
+                textTransform: 'uppercase',
+                border: 0,
+                borderBottom: '3px solid #52baf2',
+                outline: 'none',
+                color: '#ffffff',
+                float: 'none',
+                fontSize: '50px',
+                fontWeight: '300',
+                lineHeight: '1',
+                padding: '5px 0',
+                width: '100%',
+                height: '50px',
+              }}
+            />
+
+            <br />
+            <br />
+            {loading && (
+              <LinearProgress style={{ backgroundColor: '#52baf2' }} />
+            )}
+            <br />
+            <Typography variant='h5' gutterBottom>
+              {message}
+            </Typography>
+          </div>
         </header>
       </div>
-    );
+    )
   }
 }
 
